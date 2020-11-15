5e3998a6f9eda93f9e8bcf1f54a016f118a966f1
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro.local>
2020-11-15 03:30:52 -0500

    updated models and routers

--- api/users/auth-router.js
+++ api/users/auth-router.js
@@ -1,1 +1,17 @@
 const router = require('express').Router()
+const Users = require('./user-model')
+
+
+
+//get users 
+router.get("/users", async (req,res,next) => {
+    try {
+        const user = await Users.find()
+        res.json(user)
+    } catch (err) {
+        next(err)
+    }
+})
+
+
+module.exports = router
--- api/users/user-model.js
+++ api/users/user-model.js
@@ -1,1 +1,43 @@
 const db = require('../../data/config')
+
+
+module.exports = {
+  find,
+  findBy,
+  findUserById,
+  add,
+  update,
+  remove
+}
+
+function find() {
+  return db('users')
+}
+
+function findBy(filter) {
+  return db('users')
+    .where(filter)
+}
+
+function findUserById(id) {
+  return db('users')
+    .where({ id })
+    .first()
+}
+
+function add(user) {
+  return db.insert(user)
+  .into('users')
+}
+
+function update(id, changes) {
+  return db('users')
+    .where({ id })
+    .update(changes)
+}
+
+function remove(id) {
+  return db('users')
+      .where('id', id)
+      .del()
+}
