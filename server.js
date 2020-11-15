2826dbb3f2604e95191c275128d6be690189f5e8
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro.local>
2020-11-15 02:31:57 -0500

    connected server to routers and imported config file to models

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const morgan = require('morgan')
 const cors = require('cors')
 require('colors')
 
-
-
 const server = express()
+
+const plantRouter = require('./api/plant/plant-router')
+const authRouter = require('./api/users/auth-router')
+
 server.use(helmet())
 server.use(morgan('dev'))
 server.use(cors())
 server.use(express.json())
+// server.use('/api/plants', plantRouter)
+// server.use('/api/users/auth-router.js', authRouter)
 
 
 
