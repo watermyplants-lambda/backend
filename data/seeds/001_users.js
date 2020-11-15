8fc163434f4658d9ea472e0fdfaa7086a8ccf90e
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro.local>
2020-11-15 03:54:39 -0500

    added dummy user data and added plant data, end points are fetching data

--- data/seeds/001_users.js
+++ data/seeds/001_users.js
@@ -1,13 +1,14 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').delete()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+      return knex('users').insert([
+        {id: 1, firstname: 'Skelator', lastname: 'beets', email:'Skelator@greyskull.com', password: 'battlecat'},
+      
+      ])
+    })
+}
+
+//still will need to add bcrypt
--- data/seeds/002_plants.js
+++ data/seeds/002_plants.js
@@ -1,13 +1,16 @@
-
-exports.seed = function(knex) {
+  
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('plants').delete()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+      return knex('plants').insert([
+        { id: 1, user_id: 1, name: 'Agave', species: 'succulent', water_schedule: '2020-12-31 11:00', frequency: 1, last_watered: '2020-11-1', image_url: 'https://cdn.pixabay.com/photo/2017/04/25/22/28/despaired-2261021__480.jpg' },
+        { id: 2, user_id: 1, name: 'Cactus', species: 'succulent', water_schedule: '2020-12-30 09:50', frequency: 3, last_watered: '2020-11-1', image_url: 'https://cdn.pixabay.com/photo/2017/07/25/22/54/office-2539844__480.jpg' },
+        { id: 3, user_id: 2, name: 'Jeff', species: 'succulent', water_schedule: '2020-12-31 12:00', frequency: 1, last_watered: '2020-11-1', image_url: 'https://cdn.pixabay.com/photo/2016/07/20/00/06/cactus-1529343__480.jpg' },
+        { id: 4, user_id: 2, name: 'Dwight', species: 'succulent', water_schedule: '2021-01-01 10:40', frequency: 3, last_watered: '2020-11-1', image_url: 'https://cdn.pixabay.com/photo/2020/07/24/16/37/cactus-5434469__480.jpg' },
+        { id: 5, user_id: 3, name: 'Shrute', species: 'succulent', water_schedule: '2020-12-05 16:00', frequency: 1, last_watered: '2020-11-1', image_url: 'https://cdn.pixabay.com/photo/2015/11/26/00/01/cactus-1063094_1280.jpg' },
+        
+      ])
+    })
+}
